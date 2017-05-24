a5f1ee6813f955d9570661d823476594daf8639c
Suveena Thanawala <thanawal@usc.edu>
2017-05-24 12:28:01 -0700

    Usermap as hashmap

--- src/codeu/chat/UserMap.java
+++ src/codeu/chat/UserMap.java
@@ -1,6 +1,7 @@
 package codeu.chat;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 /**
@@ -13,17 +14,38 @@ If they do not, add them to the map.*/
 
 public class UserMap {
 
-    ArrayList<User> userList;
+    HashMap<String, String> map;
+
+    public UserMap() {
+        this.map = new HashMap<String, String>();
+    }
+
+   /*ArrayList<User> userList;
 
     public UserMap() {
         this.userList = new ArrayList<User>();
     }
 
     // Checks if user exists given a username (String)
-    public boolean checkUser(String name) {
+    public User checkUser(String name) {
         Iterator<User> itr = this.userList.iterator();
         while (itr.hasNext()) {
-           // if (itr.)
+            User nextUser = itr.next();
+            if(nextUser.username.equals(name)) {
+                return nextUser;
+            }
         }
+        return null;
     }
+
+    // Not yet tested -- should return true if user/pass match
+    public boolean checkPassword(String name, String pass) {
+        User targetUser = checkUser(name);
+        if (targetUser != null) {
+            if (targetUser.password == pass) {
+                return true;
+            }
+        }
+        return false;
+    }*/
 }
