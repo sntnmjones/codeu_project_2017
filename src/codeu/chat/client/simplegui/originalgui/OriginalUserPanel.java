c58c38283a80c9f88d3ee22f88c416d0ccebec16
suveenat <thanawal@usc.edu>
2017-05-30 22:30:47 -0700

    Removed before/after windows

--- src/codeu/chat/client/simplegui/originalgui/OriginalUserPanel.java
+++ src/codeu/chat/client/simplegui/originalgui/OriginalUserPanel.java
@@ -162,9 +162,7 @@ public final class OriginalUserPanel extends JPanel {
       public void actionPerformed(ActionEvent e) {
         // TODO: enable logout
         clientContext.user.signOutUser();
-        JOptionPane.showMessageDialog(new JFrame(), "before");
         landingFrame.setVisible(true);
-        JOptionPane.showMessageDialog(new JFrame(), "after");
         mainFrame.dispose();
       }
     });
